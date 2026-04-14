13a639e6a615fca94adb270def37c9f91162c07a
Mark Bryan <Mark Bryan@LAPTOP-JREKPOMM>
2026-04-14 11:34:27 +0800

    commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require
+require('dotenv').config();
 
 const express = require("express");
 const mysql = require("mysql2");
@@ -8,7 +8,7 @@ const app = express();
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   ssl: {
