7e347f2c0db8044cf9c3781bcc8eecd53b41888b
Mark Bryan <Mark Bryan@LAPTOP-JREKPOMM>
2026-04-14 12:14:56 +0800

    commit

--- app.js
+++ app.js
@@ -10,17 +10,18 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 3306,
   ssl: {
-    rejectUnauthorized: false
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true
   }
 });
 
 app.get("/", (req, res) => {
   db.query("SELECT NOW()", (err, result) => {
     if (err) {
-  console.error("DB Error:", err.message);
-    return res.status(500).send("Database connection failed");
+        console.error("DB Error:", err.message);
+        return res.status(500).send("Database connection failed");
     }
     res.send("Database Connected Successfully: " + result[0]["NOW()"]);
   });
