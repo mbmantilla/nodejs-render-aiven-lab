69c36549f1bb22e5a419fea0c2e8abc12a0c7c8b
Mark Bryan <Mark Bryan@LAPTOP-JREKPOMM>
2026-04-14 11:16:52 +0800

    commit

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const mysql = require("mysql2");
 const app = express();
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 app.get("/", (req, res) => {
