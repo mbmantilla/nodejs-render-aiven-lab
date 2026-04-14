954104cce00cbe0294017f96d5bf808ee71078ca
Mark Bryan <Mark Bryan@LAPTOP-JREKPOMM>
2026-04-14 16:45:02 +0800

    commit

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 3306,
   ssl: {
-    minVersion: "TLSv1.2",
-    rejectUnauthorized: true
+    // This is the key change: 
+    // It skips the "self-signed certificate" check for lab purposes
+    rejectUnauthorized: false 
   }
 });
 
